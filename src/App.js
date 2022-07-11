247db8a60d10f9f63a056905c9f986435591c54d
Abhishek N N <7abhisheknn@gmail.com>
2022-07-11 23:06:34 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlaces";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Users />} />
+        <Route path="/places/new" element={<NewPlace />} />
         <Route path="/*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,4 @@
+function NewPlace() {
+  return <h2> Place works</h2>;
+}
+export default NewPlace;
